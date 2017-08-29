e9e05ec325b543eadfd7c09bcc9312ce834a929f
Aashish Gaba <aashishgaba097@gmail.com>
2017-08-29 22:40:58 +0000

    Update echo.c

--- Final/echo.c
+++ Final/echo.c
@@ -4,8 +4,21 @@
 int echo(char **args)
 {
 	int i=1;
-	while(args[i]!=NULL){
+	while(args[i]!=NULL)
+	{
+	if(args[i][0]=='$')
+	{
+		const char* str=getenv(args[i]+1);
+		if(str != NULL)
+			printf("%s",str);
+		else
+			printf(" ",str);
+	
+	}
+	else{
+
 		printf("%s ",args[i]);
+	}
 		i++;
 	}
 	printf("\n");
