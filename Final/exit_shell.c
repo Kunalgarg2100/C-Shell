b5a3e038063e0577404ff5f2ff06ff1739c7dc5e
Kunalgarg2100 <kunalgarg2100@gmail.com>
2017-08-29 22:29:34 +0000

    Update exit_shell.c

--- Final/exit_shell.c
+++ Final/exit_shell.c
@@ -1,23 +1,5 @@
-#include <stdio.h> 
-#include <dirent.h>
-#include <fcntl.h>
-#include <pwd.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-//start lsa.c
-#include <sys/dir.h>
-#include <sys/param.h>
-#include <sys/stat.h>
-#include <grp.h>
-#include <time.h>
-#include <locale.h>
-#include <langinfo.h>
-#include <stdint.h>
+ #include <stdlib.h>
 
-#include "function.h"
 
 int exit_shell(char ** args)
 {
