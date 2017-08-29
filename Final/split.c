461e3684c460e8271b7c8ca1fb0349e0ae2f59b6
Kunalgarg2100 <kunalgarg2100@gmail.com>
2017-08-29 22:35:46 +0000

    Update split.c

--- Final/split.c
+++ Final/split.c
@@ -1,22 +1,6 @@
 #include <stdio.h> 
-#include <dirent.h>
-#include <fcntl.h>
-#include <pwd.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
-//start lsa.c
-#include <sys/dir.h>
-#include <sys/param.h>
-#include <sys/stat.h>
-#include <grp.h>
-#include <time.h>
-#include <locale.h>
-#include <langinfo.h>
-#include <stdint.h>
-
 #include "function.h"
 
 
