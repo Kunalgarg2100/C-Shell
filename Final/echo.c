2a678b8193a4deba27a7696457116e99b79d19c1
Kunalgarg2100 <kunalgarg2100@gmail.com>
2017-08-30 05:32:28 +0530

    added functionality in echo

--- Final/echo.c
+++ Final/echo.c
@@ -1,9 +1,44 @@
 #include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
 #include "function.h"
 
 int echo(char **args)
 {
 	int i=1;
+	if(args[i][0] == '\"' || args[i][0] == '\'')
+	{
+		int x = strlen(args[i]);
+
+		
+		if(args[i][x-1] == '\"' || args[i][x-1] == '\'')
+			for(int j=1;j<x-1;j++)
+				printf("%c",args[i][j]);
+		else
+			for(int j=1;j<x;j++)
+			printf("%c",args[i][j]);
+		printf(" ");
+
+		i++;
+
+		while(args[i]!=NULL)
+		{
+			int x = strlen(args[i]);
+			if(args[i][x-1] == '\"' || args[i][x-1] == '\'')
+			for(int j=0;j<x-1;j++)
+				printf("%c",args[i][j]);
+			else
+			printf("%s ",args[i]);
+
+
+			i++;
+
+		}
+		
+
+	}
+
+
 	while(args[i]!=NULL)
 	{
 	if(args[i][0]=='$')
@@ -12,9 +47,12 @@ int echo(char **args)
 		if(str != NULL)
 			printf("%s",str);
 		else
-			printf(" ",str);
+			printf(" ");
 	
 	}
+
+
+
 	else{
 
 		printf("%s ",args[i]);
--- Final/pwd.c
+++ Final/pwd.c
@@ -6,6 +6,6 @@ int pwd(char ** args)
 {
 	char cwd[1111];
 	getcwd(cwd,sizeof(cwd));
-	printf("\n%s\n",cwd);
+	printf("%s\n",cwd);
     return 1;
 }
